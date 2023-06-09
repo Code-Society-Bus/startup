3037dcc7a861aa8445e40cf835e80c87482214ed
Sabbadini10 <matiassabbadini24@gmail.com>
2023-06-09 14:30:34 -0300

    styles(nabvar)update style hover

--- src/app/component/about/about.component.ts
+++ src/app/component/about/about.component.ts
@@ -10,6 +10,8 @@ import Splide from '@splidejs/splide';
 })
 export class AboutComponent implements OnInit{
 
+
+
   ngOnInit() {
     let splide = new Splide( '#splide1', {
       type   : 'loop',
@@ -18,5 +20,33 @@ export class AboutComponent implements OnInit{
 
     splide.mount();
   }
+
+
+  translateValue: string = '-1086.59';
+
+  cambiarTransformacion(valor: string) {
+    // Lógica para cambiar la transformación
+    // Puedes cambiar la lógica según tus necesidades
+    if (valor === '1') {
+      this.translateValue = '-1086.59';
+    } else if (valor === '2') {
+      this.translateValue = '-1700.72';
+    } else if (valor === '3') {
+      this.translateValue = '-2318.19';
+    }
+  }
+
+ /*  currentTranslateX: number = 0;
+  moveSliderRight() {
+    this.currentTranslateX -= 0; // Cambia el valor según tus necesidades
+  }
+
+  moveSliderCenter() {
+    this.currentTranslateX -= 600; // Cambia el valor según tus necesidades
+  }
+
+  moveSliderLeft() {
+    this.currentTranslateX += 600; // Cambia el valor según tus necesidades
+  } */
 
 }
